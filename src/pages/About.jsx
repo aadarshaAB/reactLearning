398007d9f4e2f5bf89022393194e31b13a836140
aadarshaAB <aadarshabhattarai873@gmail.com>
2023-12-26 21:57:24 +0545

    learn hooks useffect usestate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React, { useState } from 'react'
+// import NavBar from '../components/NavBar'
 
 export const About = () => {
+    const[number,setNumber]=useState(0)
+
+    function increase(number){
+        setNumber(number=>number+1)
+    }
+    function decrease(number){
+        setNumber(number=>number-1)
+    }
+
   return (
     <div>
-        <NavBar/>
-        <h1>About</h1></div>
+        <h1>{number}</h1>
+        <button onClick={increase}>+</button>
+        <button onClick={decrease}>-</button>
+    </div>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Button from '../components/Button'
-import NavBar from '../components/NavBar'
+// import React from 'react'
+// import Button from '../components/Button'
+// import NavBar from '../components/NavBar'
 
+import { useEffect, useState } from "react"
+//jati khera ni code run hunxa
 export const Contact = () => {
+  // useEffect(()=>{
+  //   console.log("Hello")
+  // },[])
+  const[number,setNumber]=useState(0)
+//   //first time mount huda yo code run hunxa
+//   useEffect(()=>{
+
+//   },[])
+// //jaba yo state name ko state change hunxa
+  useEffect(()=>{
+    console.log('Hello')
+  },[number])
   return (
   
     <div>
-        <NavBar/>
+        {/* <NavBar/>
         <h1>Contact</h1>
-        <Button text="Location"/>
+        <Button text="Location"/> */}
+        <h1>{number}</h1>
+        <button onClick={()=>setNumber(number+1)}>increase</button>
         </div>
   )
 }
